dcba8f090cb77f78195afa6d57ed9f5278927530
Keri Dominik <keridodorobi@gmail.com>
2025-03-25 14:48:23 +0100

    asd

--- src/test/Login.test.tsx
+++ src/test/Login.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
 import Login from '../pages/Login';
 import '@testing-library/jest-dom';
 
 test('renders a div on the login page', () => {
-    render(<Login />);
+    render(
+        <MemoryRouter>
+            <Login />
+        </MemoryRouter>,
+    );
     const divElement = screen.getByTestId('login-div');
     expect(divElement).toBeInTheDocument();
 });
